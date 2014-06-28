3927b23eb8af0d9cccbdf8d2327853ea10ba941b
Branislav Dinic <branislavdinich@gmail.com>
2014-06-28 16:07:50 +0200

    added aliases for git add more stuff

--- Alias.bat
+++ Alias.bat
@@ -1,26 +1,42 @@
 
+
+DOSKEY alias= notepad C:\Bin\Alias.bat
+DOSKEY aliasf= cd C:\Bin
+
 ::$T     Command separator.  Allows multiple commands in a macro.
 
 ::Some commands from Unix
 
 DOSKEY ls=DIR $* 
-DOSKEY cp=COPY $* 
+::DOSKEY cp=COPY $* 
 DOSKEY xcp=XCOPY $*
 DOSKEY mv=MOVE $* 
 DOSKEY clear=CLS
 DOSKEY h=DOSKEY /HISTORY
+DOSKEY cp=chdir ^| clip
 
 ::Git shortcuts
 
 DOSKEY gs=git status
-DOSKEY gl=git log --oneline --all --graph --decorate
-DOSKEY alias=if ".$*." == ".." ( DOSKEY /MACROS ) else ( DOSKEY $* )
+::DOSKEY alias=if ".$*." == ".." ( DOSKEY /MACROS ) else ( DOSKEY $* )
 
-::Git files shortcuts
+::Template and Git files shortcuts
 
 DOSKEY temp06=XCOPY/SHE "C:\Users\Bane\Desktop\Dev\Templates\Template 1.06" ^& rmdir/s/q .git ^& git init ^& chdir ^| clip ^& start cmd ^& cd sass ^& sass -w --style expanded main.scss 
 
- 
+::Git shortcuts
+
+DOSKEY gs=git status
+DOSKEY gp=git push
+DOSKEY ga= git add .
+DOSKEY gc= git commit
+DOSKEY gl=git log --oneline --all --graph --decorate
+
+
+:: Sass commands
+
+DOSKEY sw=cd sass ^& sass -w --style expanded main.scss
+DOSKEY swn=cd sass ^& start cmd ^& sass -w --style expanded main.scss 
 
 ::Clears screen when loaded 
 cls
