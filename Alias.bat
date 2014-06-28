62b19a5c4dcff7b42f654f69caf40829ccfdd3e6
Branislav Dinic <branislavdinich@gmail.com>
2014-06-28 06:50:21 +0200

    added temp git macro

--- Alias.bat
+++ Alias.bat
@@ -1,3 +1,6 @@
+
+::$T     Command separator.  Allows multiple commands in a macro.
+
 ::Some commands from Unix
 
 DOSKEY ls=DIR $* 
@@ -12,4 +15,12 @@ DOSKEY h=DOSKEY /HISTORY
 DOSKEY gs=git status
 DOSKEY gl=git log --oneline --all --graph --decorate
 DOSKEY alias=if ".$*." == ".." ( DOSKEY /MACROS ) else ( DOSKEY $* )
+
+::Git files shortcuts
+
+DOSKEY temp06=XCOPY/SHE "C:\Users\Bane\Desktop\Dev\Templates\Template 1.06" ^& rmdir/s/q .git ^& git init ^& chdir ^| clip ^& start cmd ^& cd sass ^& sass -w --style expanded main.scss 
+
+ 
+
+::Clears screen when loaded 
 cls
